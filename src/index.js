c427bec32ea0d37838b9a3fdf1b807da2f9d5052
Peopleandlifelover <78507301+Peopleandlifelover@users.noreply.github.com>
2021-02-27 21:00:05 +0100

    corrected HTML file

--- src/index.js
+++ src/index.js
@@ -51,3 +51,48 @@ function showTime(datetime) {
   ];
   let month = months[now.getMonth()];
   let year = now.getFullYear();
+
+  document.querySelector("#clock").innerHTML = `${hour}:${minute}`;
+  document.querySelector("#day").innerHTML = day;
+  document.querySelector("#date").innerHTML = `${date} ${month} ${year}`;
+  document.querySelector("#amPm").innerHTML = amPm;
+
+  function convertHour() {
+    if (hour <= 9) {
+      hour = `0${hour}`;
+    } else if (hour > 9 && hour <= 12) {
+      return hour;
+    } else if (hour > 12 && hour <= 21) {
+      hour = hour - 12;
+      hour = `0${hour}`;
+    } else {
+      hour = hour - 12;
+    }
+  }
+}
+
+let celsius = null;
+let celsiusFeelsLike = null;
+let fahrenheit = null;
+let fahrenheitFeelsLike = null;
+let unit = null;
+
+
+function showTemperature(response) {
+  let city = response.data.name;
+  let country = response.data.sys.country;
+  let temp = response.data.main.temp;
+  let description = response.data.weather[0].description;
+  let cloudiness = Math.round(response.data.clouds.all);
+  let wind = Math.round(response.data.wind.speed);
+  let humidity = Math.round(response.data.main.humidity);
+  let feelsLike = response.data.main.feels_like;
+  let icon = document.querySelector("#icon");
+  showTime(new Date(response.data.dt * 1000));
+
+  celsius = temp;
+  celsiusFeelsLike = feelsLike;
+  fahrenheit = temp;
+  fahrenheitFeelsLike = feelsLike;
+
+
